187e7dce43bba1c33c36cefde4c7c8f3dde62a21
ajithDCKAP <41795417+DeeAjith@users.noreply.github.com>
2022-04-17 19:22:45 +0530

    frontend finish

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 // hero-switch
 $(".home-hero .__heroContent .__action").on("click", function () {
-    $('#q-cards,#q-hero').fadeIn(1000, function() {
+    $('#q-cards,#q-hero').fadeIn(1000, function () {
         // run extra code here
         $("#q-cards").switchClass("d-none", "d-block");
         $("#q-hero").switchClass("d-block", "d-none");
-      });
+    });
 });
 
 // q-cards
@@ -12,9 +12,10 @@ const swiper = new Swiper('.swiper', {
     speed: 600,
     allowTouchMove: false,
     slideShadows: false,
-    simulateTouch: false,
+    // simulateTouch: false,
     navigation: {
-        nextEl: '.swiper-slide .__action',
+        nextEl: '.swiper-slide .__action:not(.__submit)',
+        prevEl: '.swiper-slide .__previous',
     },
     effect: 'creative',
     creativeEffect: {
@@ -27,3 +28,17 @@ const swiper = new Swiper('.swiper', {
     },
 });
 $
+
+// userForm
+$('input.form-input').focus(function () {
+    $(this).parents('.field').addClass('focused');
+});
+
+$('input').blur(function () {
+    var inputValue = $(this).val();
+    if (inputValue == "") {
+        $(this).parents('.field').removeClass('focused');
+    } else {
+        $(this).addClass('filled');
+    }
+})
