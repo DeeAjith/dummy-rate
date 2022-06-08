0d3363312dd9840f11d3c629ead7e41a8c8948f7
RahilOvaiz <30799494+RahilOvaiz@users.noreply.github.com>
2022-06-08 18:08:34 +0530

    dynamic button for minutes

--- graph.php
+++ graph.php
@@ -62,7 +62,7 @@ if (isset($_POST['submit'])) {
     // print_r(json_encode($messages));
     // mail("rahiovaiz@gmail.com", "Success", "Send mail from localhost using PHP");
 } else {
-    header('Location: /');
+    header('Location: index.php');
     exit();
 }
 ?>
